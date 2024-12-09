6a0b6ab2a75d91f57317cd387c2f979a76f8b23d
jiajia.hu <jiajia.hu@rakuten.com>
2024-12-10 01:53:26 +0900

    Add more content

--- src/components/headerNav.js
+++ src/components/headerNav.js
@@ -9,9 +9,10 @@ const HeaderNav = () => {
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5 font-semibold text-gray-900">
-                        Jocelyn's life
-                    </a>
+                    {/*<a href="#" className="-m-1.5 p-1.5 font-semibold text-gray-900">*/}
+                    {/*    Jocelyn's life*/}
+                    {/*</a>*/}
+                    <Link  to="/" className="-m-1.5 p-1.5 font-semibold text-gray-900">Jocelyn's life</Link>
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
                     <div className="relative">
